f1eab3235c2768d97db6c5df5d1590d44fde9cb7
Saket121 <80620868+Saket121@users.noreply.github.com>
2022-06-10 18:36:26 +0530

    Updating the server side

---
+++ controllers/authController.js
@@ -0,0 +1,21 @@
+import User from "../models/User.js"
+
+
+const register = async (req, res) => {
+   try {
+       const user = await User.create(req.body)
+       res.status(201).json({user})
+   } catch (error) {
+       res.status(500).json({msg: 'there was an error'})
+   }
+}
+
+const login = async (req, res) => {
+    res.send('login user')
+}
+const updateUser = async (req, res) => {
+    res.send('updateUser')
+}
+
+
+export { register, login, updateUser }
